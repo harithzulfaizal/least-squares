a6f5486cdf45e1ee8a4febe11000fa071b3a7337
harithzulfaizal <56865880+harithzulfaizal@users.noreply.github.com>
2021-04-07 09:55:29 +0800

    Create LeastSqFA_Legendre.py

--- LeastSqFA_Legendre.py
+++ LeastSqFA_Legendre.py
@@ -3,36 +3,40 @@ import matplotlib.pyplot as plt
 from scipy.special import legendre
 from scipy.integrate import quad
 
-func = lambda x: np.sin(np.pi*x)
+func = lambda x: np.sin(np.pi*x) # Original function to be approximated
 orth = [lambda x: 1, lambda x: x, lambda x: x**2-(1/3), lambda x: x**3-(3/5)*x] # Test
 
 def c(f, phi2, a, b):
-    num = quad(f, a, b)
+    num = quad(f, a, b) # Integral of f, limits a to b
     dnm = quad(phi2, a, b)
-    return num[0]/dnm[0]
+    return num[0]/dnm[0] # c_i = \frac{\langle f,\phi_i \rangle}{\langle \phi_i,\phi_i \rangle}
 
 def y(order, x, coeff):
     polyn = 0
     for i in range(order + 1):
-        polyn += coeff[i] * legendre(i)(x)
+        polyn += coeff[i] * legendre(i)(x) # \hat{f}(x) = \sum_{i=1}^n c_i \phi_i(x)
     return polyn
 
-order = 3
-a, b = -1, 1
-coeff = []
-xlin = np.linspace(a, b, 1000)
+def main():
+    order = 3
+    a, b = -1, 1
+    coeff = []
+    xlin = np.linspace(a, b, 1000)
 
-for i in range(order+1):
-    phi = legendre(i)
+    for i in range(order+1):
+        phi = legendre(i) # Orthogonal function -- Legendre polynomials
 
-    f = lambda x: func(x)*phi(x)
-    phi2 = lambda x: phi(x)*phi(x)
+        f = lambda x: func(x)*phi(x) # Combining 2 functions
+        phi2 = lambda x: phi(x)*phi(x)
 
-    coeff.append(c(f, phi2, a, b))
+        coeff.append(c(f, phi2, a, b))
 
-print(coeff)
-plt.plot(xlin, func(xlin), label=r'$\sin\,{\pi x}$')
-plt.plot(xlin, y(order,  xlin, coeff), '--', label=r'Approximation')
-plt.legend()
-plt.title('Least squares function approximation of $\sin\,{\pi x}$')
-plt.show()
+    print(coeff)
+    plt.plot(xlin, func(xlin), label=r'$\sin\,{\pi x}$')
+    plt.plot(xlin, y(order,  xlin, coeff), '--', label=r'Approximation')
+    plt.legend()
+    plt.title('Least squares function approximation of $\sin\,{\pi x}$')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
